2c53df4012313ee86b9c2d9c5984653bbab4d800
Aditya Suman <sumanaditya19@gmail.com>
2020-01-15 00:27:14 +0530

    added tech nd serveics

--- src/app/landing/pages/home/home.component.ts
+++ src/app/landing/pages/home/home.component.ts
@@ -67,9 +67,133 @@ export class HomeComponent implements OnInit, AfterViewInit {
     'Innovation'
   ]
 
+  public detailarray = [
+    'Have an IDEA? Bring it to the world with the help of our experienced developers who dedicatedly work with you to bring out the best version of your product.',
+    'Be it SEO, Digital Marketing or just some minor tweaks to rank your website, Our specialized team of digital marketing experts can handle it all.',
+    'Drones? AI? Blockchain and Machine Learning? We love these words.. Be it autonomous flights to wearable tech, Our team of Data Scientists and Developers can build all the Algorithms.'
+  ]
+
+  public services = [
+    {
+      name: 'Static Websites',
+      url: ''
+    },
+    {
+      name: 'Dynamic Websites',
+      url: ''
+    },
+    {
+      name: 'Web Apps',
+      url: ''
+    },
+    {
+      name: 'Progressive Web Apps',
+      url: ''
+    },
+    {
+      name: 'Hybrid Apps',
+      url: ''
+    },
+    {
+      name: 'Android Apps',
+      url: ''
+    },
+    {
+      name: 'IOS Apps',
+      url: ''
+    },
+    {
+      name: 'Desktop Apps',
+      url: ''
+    },
+    {
+      name: 'Backend API Development',
+      url: ''
+    },{
+      name: 'Frontend Development',
+      url: ''
+    },{
+      name: 'API Integration',
+      url: ''
+    },
+    {
+      name: 'Wearable Tech',
+      url: ''
+    },
+    {
+      name: 'Data Science',
+      url: ''
+    },
+    {
+      name: 'Machine Learning',
+      url: ''
+    },
+    {
+      name: 'Image Processing',
+      url: ''
+    },
+    {
+      name: 'AR/VR',
+      url: ''
+    },
+    {
+      name: 'UI/UX Design',
+      url: ''
+    },
+    {
+      name: 'Arduino',
+      url: ''
+    },
+    {
+      name: 'Raspberry-Pi',
+      url: ''
+    },
+    {
+      name: 'Drones/Robotics',
+      url: ''
+    },
+    {
+      name: 'Automation',
+      url: ''
+    },
+    {
+      name: 'Sensors and Microprocessors',
+      url: ''
+    },
+    {
+      name: 'CAD Designing',
+      url: ''
+    },
+    {
+      name: 'E-Commerce',
+      url: ''
+    },
+    {
+      name: 'Wordpress',
+      url: ''
+    },
+    {
+      name: 'SEO',
+      url: ''
+    },
+    {
+      name: 'Digital Marketing',
+      url: ''
+    },
+    {
+      name: 'Website Optimization',
+      url: ''
+    },
+    {
+      name: 'Maintenance',
+      url: ''
+    },
+  ]
+
   public head = '';
   public head2 = '';
   public id = '';
+  public detail = '';
 
   public callHide = () => {
     this.hide = true;
@@ -82,15 +206,16 @@ export class HomeComponent implements OnInit, AfterViewInit {
     }, 400);
   }
 
-  public index = 0;
+  public index = -1;
   public start = 100;
   public time = () => {
+    this.callHide();
     setTimeout(() => {
       this.head = this.headarray[this.index];
       this.head2 = this.headarray2[this.index];
       this.id = this.idarray[this.index];
-      this.callHide();
-      if (this.index == 1) {
+      this.detail = this.detailarray[this.index];
+      if (this.index == 0) {
         this.start = 7000;
       }
       // Then recall the parent function to
