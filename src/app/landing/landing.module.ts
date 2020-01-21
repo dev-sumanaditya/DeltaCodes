ce543d04f0ce1e3a6f2ae020e71a95a71465fb0b
Aditya Suman <sumanaditya19@gmail.com>
2020-01-21 19:52:13 +0530

    added slider

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingRoutingModule } from './landing-routing.module';
+import { DragScrollModule } from 'ngx-drag-scroll';
+
 import { PanelComponent } from './panel/panel.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LandingRoutingModule } from './landing-routing.module';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SliderbgComponent } from './pages/components/sliderbg/sliderbg.component';
+
 
 @NgModule({
   declarations: [PanelComponent, HomeComponent, ContactComponent, AboutComponent, SliderbgComponent],
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    DragScrollModule
   ]
 })
 export class LandingModule { }
