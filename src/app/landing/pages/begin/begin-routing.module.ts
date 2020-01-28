01072261f8c4557614dc897c6c7977d9b4ab57f2
Aditya Suman <sumanaditya19@gmail.com>
2020-01-28 20:38:04 +0530

    added begin sub route

--- src/app/landing/pages/begin/begin-routing.module.ts
+++ src/app/landing/pages/begin/begin-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PanelComponent } from './panel/panel.component';
+import { WebComponent } from './pages/web/web.component';
+import { ServComponent } from './pages/serv/serv.component';
+import { TechComponent } from './pages/tech/tech.component';
 
 const routes: Routes = [
-  {path: '', component: PanelComponent}
+  {path: '', component: PanelComponent, children: [
+    {path: 'services', component: ServComponent},
+    {path: 'tech', component: TechComponent},
+    {path: '', component: WebComponent},
+  ]}
 ];
 
 @NgModule({
