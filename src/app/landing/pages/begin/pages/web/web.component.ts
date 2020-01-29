2fd4252f59874ef82e1fc02b427f0b7805788d93
Aditya Suman <sumanaditya19@gmail.com>
2020-01-29 20:28:34 +0530

    edited

--- src/app/landing/pages/begin/pages/web/web.component.ts
+++ src/app/landing/pages/begin/pages/web/web.component.ts
@@ -12,4 +12,17 @@ export class WebComponent implements OnInit {
   ngOnInit() {
   }
 
+  public showM1: boolean = true;
+  public showM2: boolean = false;
+  public loading: boolean = false;
+  public submit(data) {
+    console.log(data);
+    this.showM1 = false;
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this.showM2 = true;
+    }, 1000);
+  }
 }
