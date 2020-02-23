90eaac3db96279bf47063ce7650f9bd9dc9ab823
Aditya Suman <sumanaditya19@gmail.com>
2020-02-23 18:19:55 +0530

    ana fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+          gtag('config', 'xx-xxxxx-xx',
+                {
+                  'page_path': event.urlAfterRedirects
+                }
+          );
+      }
+    });
+  }
+}
