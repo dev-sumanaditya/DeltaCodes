87bd28f70f0760416a4ae74badba5f7b550cb57d
Aditya Suman <sumanaditya19@gmail.com>
2020-02-28 15:38:55 +0530

    module import fixed - ngx-dragscrl

--- src/app/landing/pages/homeModule/home.module.ts
+++ src/app/landing/pages/homeModule/home.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SliderbgComponent } from './components/sliderbg/sliderbg.component';
-import { DragScrollModule } from 'ngx-drag-scroll';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent, SliderbgComponent],
   imports: [
     CommonModule,
-    DragScrollModule,
     HomeRoutingModule
   ]
 })
