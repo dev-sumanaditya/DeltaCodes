517475e23d067be7ad3cf78c1240051c68eb4b26
Aditya Suman <sumanaditya19@gmail.com>
2020-04-06 11:04:05 +0530

    added industries page

--- src/app/landing/pages/industries/pages/default/default.component.ts
+++ src/app/landing/pages/industries/pages/default/default.component.ts
@@ -7,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
+  public dummy = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+  public industries = [
+    {
+      name: 'Logistics & Transportation',
+      icon1: 'logistics',
+      url: 'logistics'
+    },
+    {
+      name: 'Oil & Gas',
+      icon1: 'oil',
+      url: 'oil'
+    },
+    {
+      name: 'Telecom & Internet',
+      icon1: 'telecom',
+      url: 'telecom'
+    },
+    {
+      name: 'Tech & Innovation (R&D)',
+      icon1: 'tech',
+      url: 'tech'
+    },
+    {
+      name: 'Financial Services',
+      icon1: 'financial',
+      url: 'finance'
+    },
+    {
+      name: 'Healthcare & Life Sciences',
+      icon1: 'healthcare',
+      url: 'healthcare'
+    },
+    {
+      name: 'Travel & Hospitality',
+      icon1: 'travel',
+      url: 'travel'
+    },
+    {
+      name: 'Retail & Ecommerce',
+      icon1: 'retail',
+      url: 'retail'
+    },
+    {
+      name: 'Education & Digital Learning',
+      icon1: 'education',
+      url: 'education'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
