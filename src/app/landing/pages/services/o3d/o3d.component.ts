4e95c7147d49fa4ee3ac6d3b0c183febae15786b
Aditya Suman <sumanaditya19@gmail.com>
2020-04-09 12:07:16 +0530

    added components to all services

--- src/app/landing/pages/services/o3d/o3d.component.ts
+++ src/app/landing/pages/services/o3d/o3d.component.ts
@@ -14,6 +14,29 @@ export class O3dComponent implements OnInit {
     moreURL: 'case-studies'
   };
 
+  public faq = [
+    {
+      que: 'What are the offerings of DeltaCodes?',
+      // tslint:disable: max-line-length
+      ans: 'The development of reliable and scalable software solutions for any OS, browser and device. We bring together deep industry expertise and the latest IT advancements to deliver custom solutions and products that perfectly fit the needs and behavior of their users.'
+    },
+    {
+      que: 'What are the offerings of DeltaCodes?',
+      // tslint:disable: max-line-length
+      ans: 'The development of reliable and <a href="/">scalable software</a> solutions for any OS, browser and device. We bring together deep industry expertise and the latest IT advancements to deliver custom solutions and products that perfectly fit the needs and behavior of their users.'
+    },
+    {
+      que: 'What are the offerings of DeltaCodes?',
+      // tslint:disable: max-line-length
+      ans: 'The development of reliable and scalable software solutions for any OS, browser and device. We bring together deep industry expertise and the latest IT advancements to deliver custom solutions and products that perfectly fit the needs and behavior of their users.'
+    },
+    {
+      que: 'What are the offerings of DeltaCodes?',
+      // tslint:disable: max-line-length
+      ans: 'The development of reliable and scalable software solutions for any OS, browser and device. We bring together deep industry expertise and the latest IT advancements to deliver custom solutions and products that perfectly fit the needs and behavior of their users.'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
